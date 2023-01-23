fbff8f53463d6068a32f52f4158cbf3b1a12a557
skyeyestang99 <tommytang9958@gmail.com>
2023-01-23 15:03:40 -0800

    first commit

---
+++ app/src/main/java/ucsd/edu/cse110/cse110lab2/MainActivity.java
@@ -0,0 +1,14 @@
+package ucsd.edu.cse110.cse110lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
