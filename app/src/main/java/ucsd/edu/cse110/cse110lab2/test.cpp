1c43721918522202db41d0b282d16e3335d62979
skyeyestang99 <tommytang9958@gmail.com>
2023-01-23 15:26:40 -0800

    first commit

---
+++ app/src/main/java/ucsd/edu/cse110/cse110lab2/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tommy Tang on 1/23/23.
+//
+
+#include "test.h"
---
+++ app/src/main/java/ucsd/edu/cse110/cse110lab2/test.h
@@ -0,0 +1,16 @@
+//
+// Created by Tommy Tang on 1/23/23.
+//
+
+#ifndef CSE110LAB2_TEST_H
+#define CSE110LAB2_TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //CSE110LAB2_TEST_H
